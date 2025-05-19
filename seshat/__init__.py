0ac0f46afb05b1b4a591628525f305b779180e6b
Joan Sala <contact@joansala.com>
2025-05-19 10:13:11 +0200

    Adds support for translations

--- seshat/__init__.py
+++ seshat/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-import gi
+import sys, gi
 gi.require_version("Gtk", "4.0")
 from gi.repository import GLib
 from pathlib import Path
+from .i18n import setup_locale
 
 APP_ID = "com.joansala.Seshat"
 APP_NAME = "seshat"
@@ -12,5 +13,12 @@ APP_NAME = "seshat"
 BASE_DIR = Path(__file__).resolve().parent
 CONFIG_DIR = Path(GLib.get_user_config_dir()) / APP_NAME
 RESOURCES_DIR = BASE_DIR / "resources"
+LOCALES_DIR = BASE_DIR / "locales"
+
+try:
+    setup_locale(APP_NAME, str(LOCALES_DIR))
+except Exception as e:
+    message = f"Error setting up localization: {str(e)}"
+    print(message, file=sys.stderr)
 
 __version__ = "0.1.0"
